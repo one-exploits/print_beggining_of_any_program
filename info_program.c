be27068c6a76d5ce0be789213d0d564853f37661
One-Exploit's <moarquam997320@gmail.com>
2021-01-28 22:09:58 +0530

    info version og programm date and my bio
    info version og programm date and my bio

---
+++ info_program.c
@@ -0,0 +1,20 @@
+
+static void info(){
+printf("\n\n\nAuthor : One-Exploits @GNU/Projects"
+"\nÂ©Mohammad Arquam(C.E.O)"
+"\n+______________________________________________+"
+"\n| Programmer,H@cker,Web developer,Web designer,|\n| Data Scientist,Software developer,YouTuber,  |\n| Blogger and more.\t\t\t       |\n|\t\t\t\t\t       |"
+"\n| Gmail: oneexploits@gmail.com\t\t       |"
+"\n| GitHub: https://github.com/one-exploits      |"
+"\n| Insta: https://www.instagram.com/one_exploits\|"
+"\n| Website: https://one-exploits.business.site/ |"
+"\n|\t\t\t\t\t       |"
+"\n| \t\t\tVersion:1.00.00\t       |\n"
+"|\t\t\tDate:28/01/2021,Thu    |"
+"\n+______________________________________________+");
+
+}
+int main(){
+info();	
+	
+}
